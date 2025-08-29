ed964c95bdc70827cc1d7eeee8d243afbba6755d
enrlzzz <enricolg16@gmail.com>
2025-08-28 22:48:18 -0300

    primeiro commit de uma app em java que junta frases usando a variavel String

---
+++ TreinoStrings.java
@@ -0,0 +1,20 @@
+package treinostrings;
+
+
+public class TreinoStrings {
+
+    
+    public static void main(String[] args) {
+        
+        String nome = "Hugo";
+        String gasto = "gastou 50 reais";
+        String tempo = "ontem";
+
+       
+        String fraseCompleta = nome + " " + gasto + " " + tempo + ".";
+
+        
+        System.out.println(fraseCompleta);
+    }
+    
+}
